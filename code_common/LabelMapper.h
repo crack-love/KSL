6fcd98454a54c7d6deeedaedb2f356372a4d556e
crack-love <masterchip@naver.com>
2018-04-20 05:42:00 +0900

    코드 수정 완료 (키넥트 시험 전)

--- code_common/LabelMapper.h
+++ code_common/LabelMapper.h
@@ -1,14 +1,22 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
 using namespace std;
 
+#include "defines.hpp"
+
 #define LABEL LabelMapper::label
 
+// 싱글톤 패턴
+// LABEL(arg) 로 string, integer간 상호 전환
 class LabelMapper
 {
 private :
+	const string FILENAME = "../data/LABEL.txt";
+
 	static LabelMapper instance;
 
 	map<int, string> itosLabel;
@@ -19,10 +27,10 @@ public:
 	static int label(string s);
 
 private:
-	LabelMapper();
+	LabelMapper(); // load 호출
 
+	void load();
 	void addMap(int i, string s);
 	string lswap(int i);
 	int lswap(string s);
-
 };
