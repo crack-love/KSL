69150e32e96eace2e93887421f21566eedff8036
crack-love <masterchip@naver.com>
2018-04-23 21:18:23 +0900

    실행 가능함. 런타임 3dface 디펜던시 확인 필요

---
+++ code/common/LabelMapper.cpp
@@ -0,0 +1,70 @@
+#include "LabelMapper.h"
+
+// ½ºÅÂÆ½ »ý¼º
+LabelMapper LabelMapper::instance;
+
+LabelMapper::LabelMapper()
+{
+
+}
+
+void LabelMapper::initialize()
+{
+	//itosLabel.erase(itosLabel.begin(), itosLabel.end());
+	//stoiLabel.erase(stoiLabel.begin(), stoiLabel.end());
+	
+	load();
+}
+
+void LabelMapper::load()
+{
+	fstream is;
+	int id;
+	string name;
+
+	is.open(FILE_PATH, fstream::in);
+
+	cout << "Loading " + FILE_PATH + " ... " << is.is_open() << endl;
+	FAIL_STOP(is.is_open(), FILE_PATH + " open fail");
+
+	while (is.good())
+	{
+		is >> id >> name;
+		cout << ">>> " << id << ", " << name << endl;
+
+		addMap(id, name);
+	}
+	is.close();
+}
+
+string LabelMapper::lswap(int i)
+{
+	return itosLabel[i];
+}
+
+int LabelMapper::lswap(string s)
+{
+	return stoiLabel[s];
+}
+
+
+LabelMapper* LabelMapper::getInstance()
+{
+	return &instance;
+}
+
+string LabelMapper::label(int i)
+{
+	return itosLabel[i];
+}
+
+int LabelMapper::label(string s)
+{
+	return stoiLabel[s];
+}
+
+void LabelMapper::addMap(int i, string s)
+{
+	itosLabel[i] = s;
+	stoiLabel[s] = i;
+}
---
+++ dependency/README.md
@@ -0,0 +1,7 @@
+# PATH 설정
+
+1. 빌드를 위해서 VC15(Visual studio 2017)가 필요하다. [download](https://docs.microsoft.com/ko-kr/visualstudio/install/install-visual-studio)
+
+1. exe 프로그램 실행을 위해서 환경변수 `PATH` 설정이 필요하다. 환경변수 `PATH`에 dependency\path 폴더를 추가해야 한다. `(예 : C:\KSL\dependency\path)`
+
+1. exe 프로그램에서는 vc 재배포 패키지 2014도 필요하다. [download](\vcredist_x64.exe)
