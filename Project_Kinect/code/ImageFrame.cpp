3f7a96ae9fc22355bc5878719d246890b1b65b2f
Jeonghwan_Lee <leejeonghwan1324@gmail.com>
2018-05-15 16:30:12 +0900

    hand tracking save

---
+++ Project_Kinect/code/ImageFrame.cpp
@@ -0,0 +1,28 @@
+#include "ImageFrame.h"
+
+ImageFrame::ImageFrame()
+{
+}
+
+void ImageFrame::memorize(cv::Mat image, TIMESPAN endtime)
+{
+	// this->image = image;
+	image.copyTo(this->image);
+	this->lastFrameRelativeTime = endtime;
+}
+
+void ImageFrame::save(string filename)
+{
+	cv::imwrite(filename, image);
+}
+
+
+TIMESPAN ImageFrame::getTime()
+{
+	return this->lastFrameRelativeTime;
+}
+
+void ImageFrame::LerpMe(float p, const ImageFrame & right)
+{
+	cv::addWeighted(image, 1.0 - p, right.image, p, 0.0, image);
+}
---
+++ Project_Kinect/code/ImageFrame.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Kinect.h>
+#include <sstream>
+#include <array>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+#include "common/defines.hpp"
+
+// 어떤 부위(손) 기준 distance 계산할 것인지는 defines.hpp에 정의돼있다
+// todo : JointType -> Spoint
+
+class ImageFrame
+{
+private:
+	cv::Mat image;
+	TIMESPAN lastFrameRelativeTime;
+
+public:
+	ImageFrame();
+
+	void memorize(cv::Mat image, TIMESPAN endtime);
+
+	// ForFile
+	// string toString(int noting);
+	void save(string filename);
+
+	TIMESPAN getTime();
+
+	void LerpMe(float p, const ImageFrame &right);
+
+private:
+};
