60f6da5f23b793d5f63a036d2a3ed213a06e62ff
crack-love <masterchip@naver.com>
2018-05-27 22:39:43 +0900

    merge model finalize

--- Project_DNN/do_Learning.py
+++ Project_DNN/do_Learning.py
@@ -31,61 +31,48 @@ path.ifNoDirThanMakeDir('weight')
 # configs
 epochs = util.inputInt('How many epochs?')
 b1_batch_size = 15 # GPU 메모리 부족으로 Batch_size에 한계 있음
-b2_batch_size = 4
-m1_batch_size = 4
+b2_batch_size = 15
+m1_batch_size = 15
 isLoadWeight = util.inputInt('Load weight? (1 to yes)')
 overwrite = True
-isGuidenceTrain = util.inputInt('Guidence Train? (1 to yes)')
 
 # load dataset
 util.showProcess('Loading dataset')
 dirPath_train = path.get('data') + '\\ConvLSTM_train'
 dirPath_test = path.get('data') + '\\ConvLSTM_test'
 
-print('Loading Train Data ...')
+print('Loading Train Samples : ')
 spointList_train, roiSampleList_train, labelList_train = \
     df.ROI_loadDataListAll(dirPath_train, isShow=False, isShuffle=False)
-print('Loading Test Data ...')
+print('Loading Test Samples : ')
 spointList_test, roiSampleList_test, labelList_test = \
     df.ROI_loadDataListAll(dirPath_test, isShow=False, isShuffle=False)
 
 # write data plot graph
-print('Writing Data Plot Graph ...')
 dp.plotSpointDataList(dirPath_train, 2, True, False, True, path.get('img') + '\\d_graph.jpg')
 dp.plotSpointDataList(dirPath_train, 2, False, True, True, path.get('img') + '\\d_image.jpg')
 
 # make model
+util.showProcess('Model Generating')
 b1 = models.Model_B1()
+print('B1 done')
 b2 = models.Model_B2()
+print('B2 done')
 m1 = models.Model_M1(b1, b2)
+print('M1 done')
+m1.summary()
 
 # plot to file
-util.showProcess('Saving Model Description')
-models.saveModelDescription(b1, path.get('img'), True)
-models.saveModelDescription(b2, path.get('img'), True)
-models.saveModelDescription(m1, path.get('img'), True)
+models.saveModelDescription(b1, path.get('img'), False)
+models.saveModelDescription(b2, path.get('img'), False)
+models.saveModelDescription(m1, path.get('img'), False)
 
 # Load Weight
 if isLoadWeight == 1:
     util.showProcess('Loading Weight')
-    models.loadWeight(b1, path.get('weight'))
-    models.loadWeight(b2, path.get('weight'))
     models.loadWeight(m1, path.get('weight'))
 
 # Train
-if isGuidenceTrain == 1:
-    util.showProcess('Train B1')
-    b1.fit(spointList_train, labelList_train,
-        epochs=int(10),
-        verbose=1,
-        batch_size=b1_batch_size)
-        
-    util.showProcess('Train B2')
-    b2.fit(roiSampleList_train, labelList_train,
-        epochs=int(10),
-        verbose=1,
-        batch_size=b2_batch_size)
-
 util.showProcess('Train M1')
 m1.fit([spointList_train, roiSampleList_train], [labelList_train],
     epochs=int(epochs),
@@ -100,23 +87,7 @@ print('Test score:', score)
 print('Test accuracy:', acc)
 
 # Test
-util.showProcess('Test b1')
-accuracy = train.calculateAccuracy(spointList_test,
-                                   labelList_test,
-                                   len(labelList_test),
-                                   b1, verbose=1,
-                                   batch_size=b1_batch_size)
-print('Accuracy: ' + str(accuracy))
-
-util.showProcess('Test b2')
-accuracy = train.calculateAccuracy(roiSampleList_test,
-                                   labelList_test,
-                                   len(labelList_test),
-                                   b2, verbose=1,
-                                   batch_size=b2_batch_size)
-print('Accuracy: ' + str(accuracy))
-
-util.showProcess('Test m1')
+util.showProcess('Test M1')
 accuracy = train.calculateAccuracy([spointList_test, roiSampleList_test],
                                    labelList_test,
                                    len(labelList_test),
@@ -127,8 +98,6 @@ print('Accuracy: ' + str(accuracy))
 # Write Weight
 if overwrite:
     util.showProcess('Saving Weight')
-    models.saveWeight(b1, path.get('weight'))
-    models.saveWeight(b2, path.get('weight'))
     models.saveWeight(m1, path.get('weight'))
 
 sess.close()
--- Project_DNN/scripts/train.py
+++ Project_DNN/scripts/train.py
@@ -8,15 +8,25 @@ def calculateAccuracy(dataList, labelList, size, model, verbose, batch_size):
     '''print(model.predict(x=dataList, verbose=verbose, batch_size=batch_size))
     '''
     predicted = model.predict(x=dataList, verbose=verbose, batch_size=batch_size)
-    predicted_fit = np.argmax(predicted, 1)
+    predicted_oneHot = np.argmax(predicted, 1)
     predicteShouldBeList = np.argmax(labelList, 1)
 
-    print(predicted)
+    print('Percent:')
+    _printFloatViewable(predicted)
     print('Source:')
     print(predicteShouldBeList)
     print('Predicted:')
-    print(predicted_fit)
+    print(predicted_oneHot)
 
-    accuracy = np.sum(np.equal(predicted_fit, predicteShouldBeList)) / size
+    accuracy = np.sum(np.equal(predicted_oneHot, predicteShouldBeList)) / size
 
     return accuracy
+
+def _printFloatViewable(plist):
+    sampleSize=len(plist)
+    labelSize=len(plist[0])
+    for i in range(sampleSize):
+        for j in range(labelSize):
+            plist[i][j] = round(plist[i][j], 2)
+    
+    print(plist)
